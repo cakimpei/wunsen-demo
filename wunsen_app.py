a43c37c79ff928bf2c490e1d4753f4057aaa3c61
cakimpei <64963958+cakimpei@users.noreply.github.com>
2022-07-14 15:26:18 +0700

    Add Mandarin option

--- wunsen_app.py
+++ wunsen_app.py
@@ -5,13 +5,37 @@ from wunsen import ThapSap
 st.title('Wunsen Demo')
 
 lang_option = st.selectbox('เลือกภาษา',
-    ('ญี่ปุ่น | Japanese', 'เกาหลี | Korean', 'เวียดนาม | Vietnamese'))
+    ('จีนกลาง | Mandarin', 'ญี่ปุ่น | Japanese',
+    'เกาหลี | Korean', 'เวียดนาม | Vietnamese'))
 
 need_romaji = False
+need_sandhi = True
 
 match lang_option:
+    case 'จีนกลาง | Mandarin':
+        lang_selected = 'zh'
+        input_selected = 'Pinyin-number'
+        zh_option = st.radio('รูปแบบการทับศัพท์',
+            ('หลักเกณฑ์การทับศัพท์ภาษาจีน',
+            'เกณฑ์การถ่ายทอดเสียงภาษาจีนแมนดารินด้วยอักขรวิธีไทย'))
+        match zh_option:
+            case 'หลักเกณฑ์การทับศัพท์ภาษาจีน':
+                system_selected = 'RI49'
+                placeholder = 'ni3 hao3'
+            case 'เกณฑ์การถ่ายทอดเสียงภาษาจีนแมนดารินด้วยอักขรวิธีไทย':
+                system_selected = 'THC43'
+                placeholder = 'ni3 hao3'
+        sandhi_option = st.radio(
+            'การเปลี่ยนวรรณยุกต์เสียงสามที่อยู่หน้าเสียงสามเป็นเสียงสอง',
+            ('เปลี่ยน', 'ไม่เปลี่ยน'))
+        match sandhi_option:
+            case 'เปลี่ยน':
+                need_sandhi = True
+            case 'ไม่เปลี่ยน':
+                need_sandhi = False
     case 'ญี่ปุ่น | Japanese':
         lang_selected = 'ja'
+        system_selected = 'ORS61'
         ja_option = st.radio('ตัวเลือก',
             ('Hepburn romanization', 'Hepburn แบบไม่มี macron',
             'ญี่ปุ่น (ทดลอง)'))
@@ -29,17 +53,24 @@ match lang_option:
     case 'เกาหลี | Korean':
         lang_selected = 'ko'
         input_selected = 'RR'
+        system_selected = 'RI55'
         placeholder = 'annyeonghaseyo'
     case 'เวียดนาม | Vietnamese':
         lang_selected = 'vi'
         input_selected = 'VA'
+        system_selected = 'RI55'
         placeholder = 'xin chào'
 
 text = st.text_area('Input', max_chars=700, placeholder=placeholder)
 
 if text is not None:
     
-    thap_sap = ThapSap(lang_selected, input=input_selected)
+    thap_sap = ThapSap(lang_selected, system=system_selected,
+        input=input_selected)
+    
+    if lang_selected == 'zh':
+        thap_sap = ThapSap(lang_selected, system=system_selected,
+            input=input_selected, option={'sandhi': need_sandhi})
 
     text_split = text.splitlines()
     
@@ -54,6 +85,8 @@ if text is not None:
 st.caption("""### หมายเหตุ
 
 หน้านี้เป็นตัวทดลองของ [wunsen](https://github.com/cakimpei/wunsen) ควรตรวจสอบผลลัพธ์ก่อนนำไปใช้
+
+wunsen ไม่มีส่วนเกี่ยวข้องกับผู้กำหนดแนวทางการทับศัพท์แต่อย่างใด
 
 ตัวเลือก "ญี่ปุ่น (ทดลอง)" ใช้ [Cutlet](https://github.com/polm/cutlet) เปลี่ยนจากญี่ปุ่นเป็นโรมาจิ แล้วค่อยเปลี่ยนเป็นไทย
 """)
